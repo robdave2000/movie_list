4d96291398f044a4b277243fb41840e663e02d53
Robert Davis <robdave22@yahoo.com>
2021-11-10 16:40:50 -0500

    Beginner lab done

--- Lab/js-interactivity/js-interactivity/index.js
+++ Lab/js-interactivity/js-interactivity/index.js
@@ -1,1 +1,50 @@
-console.log("Hello World");
+console.log("JS connected");
+
+let message = document.querySelector("#message");
+
+document.querySelector("form").addEventListener("submit", addMovie);
+
+function addMovie(event)
+{
+    event.preventDefault();
+    let inputField = document.querySelector("input");
+    let movie = document.createElement("li");
+    let movieTitle = document.createElement("span");
+    movieTitle.textContent = inputField.value;
+
+    movieTitle.addEventListener("click", crossOffMovie);
+
+    movie.appendChild(movieTitle);
+    
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    
+    deleteBtn.addEventListener("click", deleteMovie);
+    
+    movie.appendChild(deleteBtn);
+    
+    document.querySelector("ul").appendChild(movie);
+
+    inputField.value = "";
+}
+
+function deleteMovie(event)
+{
+    event.target.parentNode.remove();
+
+    message.textContent = "Movie Deleted!";
+}
+
+function crossOffMovie(event)
+{
+    event.target.classList.toggle("checked");
+
+    if (event.target.classList.contains("checked"))
+    {
+        message.textContent = "Movie Watched!";
+    }
+    else
+    {
+        message.textContent = "Movie Added Back!";
+    }
+}
